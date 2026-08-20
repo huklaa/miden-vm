181fdaced606e62a4839008167a27c208624f606
hukla <129692708+huklaa@users.noreply.github.com>
2026-08-20 15:00:26 +0300

    fix(assembly): print locals attribute as integer

--- crates/assembly-syntax/src/ast/procedure/procedure.rs
+++ crates/assembly-syntax/src/ast/procedure/procedure.rs
@@ -288,7 +288,7 @@ impl crate::prettier::PrettyPrint for Procedure {
             doc += const_text("begin");
         } else {
             if self.num_locals > 0 {
-                doc += text(format!("@locals(\"{}\")", self.num_locals)) + nl();
+                doc += text(format!("@locals({})", self.num_locals)) + nl();
             }
             match self.signature() {
                 Some(sig) if sig.cc != crate::ast::types::CallConv::Fast => {
